451d49f4bf238e556313d7ab742158de89f004bb
Thalisson <thalissonpop91@gmail.com>
2023-11-20 18:06:17 -0300

    first commit

---
+++ src/main/java/com/devthalisson/eventsystem/EventsystemApplication.java
@@ -0,0 +1,13 @@
+package com.devthalisson.eventsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devthalisson/eventsystem/EventsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthalisson.eventsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
